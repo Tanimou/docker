708b57e07eb31391f54cfe8b78556ce31e5ab7ee
Tanimou Cisse <tanimou@live.fr>
2022-08-21 12:52:26 +0000

    2nd stage

--- addons/car_management/__manifest__.py
+++ addons/car_management/__manifest__.py
@@ -4,12 +4,13 @@
     'name': 'Car Management',
     'version': '1.0.0',
     'summary': 'An application for car management',
-    'description': """""",
+    'description': """This module is an application for car management. It supports the following features:
+    -Creation of cars""",
     'author': 'Progistack',
     'category': 'car',
     'sequence': -100,
     'depends': [],
-    'data': ['views/menu.xml'],#used to import all xml files
+    'data': ['views/menu.xml','views/viewscardetails.xml','views/viewsticketdetails.xml','views/viewstraveldetails.xml'],#used to import all xml files
     'demo': [],#demo database
     'installable': True,
     'auto_install': False,
