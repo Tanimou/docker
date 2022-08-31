0b147631748c94df41314175562a8534e30f8c91
Tanimou Cisse <tanimou@live.fr>
2022-08-31 18:14:37 +0000

    addmin Routing functionnality

--- addons/car_management/static/src/js/main.js
+++ addons/car_management/static/src/js/main.js
@@ -4,12 +4,12 @@ var zoomlet
     var mainLayer
     if(test===undefined){
     var test = {
-        lat: 5.395609,
-        lng: -3.978498
+        lat: 5.393937,
+        lng: -3.976591
     }
 }
 if (zoomlet === undefined) {
-    var zoomlet = 5;
+    var zoomlet = 15;
 }
     
 mapp=document.querySelector('#map')
@@ -18,7 +18,7 @@ var map = L.map(mapp).setView([test.lat, test.lng], zoomlet);
 if (mainLayer === undefined) {
  
     const mainlayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 5,
+    maxZoom: 25,
     attribution: '© OpenStreetMap'
     });
     mainlayer.addTo(map);
@@ -27,9 +27,30 @@ if (mainLayer === undefined) {
 
 
 
-var marker = L.marker([5.5, -3.97]).addTo(map);
-var marker2 = L.marker([7.5, -1.97]).addTo(map);
-var marker3 = L.marker([10.5, -8.97]).addTo(map);
+var marker = L.marker([5.393937, -3.976591]).addTo(map);
+//var marker2 = L.marker([5.393393, -3.976322]).addTo(map);
+L.Routing.control({
+    waypoints: [
+        L.latLng(5.393937, -3.976591),
+        L.latLng(5.393393, -3.976322)
+    ]
+}).addTo(map);
+// for (let index = 0; index < 6; index++) {
+//     var randomNumX = Math.random() * (5.393393 - 5.393937) + 5.393937;
+//     var randomNumY = Math.random() * (-3.976322 + 3.976591) - 3.976591;
+//     console.log(randomNumX, randomNumY);
+//     X = Math.round(randomNumX * 10000) / 10000
+//     Y = Math.round(randomNumY * 10000) / 10000
+// var circle = L.circle([X,Y ], {
+//     color: 'blue',
+//     fillColor: 'blue',
+//     fillOpacity: 0.5,
+//     radius: 2
+// }).addTo(map);
+//     circle.bindPopup(X + " " + Y);
+// }
+
+//var marker3 = L.marker([10.5, -8.97]).addTo(map);
     // }
 
 
